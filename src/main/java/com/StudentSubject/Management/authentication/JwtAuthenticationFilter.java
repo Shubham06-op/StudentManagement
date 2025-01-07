a495adecd25417b4b73515cddc904a115c58f397
Shubham Rajpure <109275761+Shubham06-op@users.noreply.github.com>
2025-01-08 00:10:27 +0530

    Update JwtAuthenticationFilter.java

--- src/main/java/com/StudentSubject/Management/authentication/JwtAuthenticationFilter.java
+++ src/main/java/com/StudentSubject/Management/authentication/JwtAuthenticationFilter.java
@@ -25,16 +25,14 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
         try {
-            // Parse credentials from the request body
+          
             Map<String, String> credentials = new ObjectMapper().readValue(request.getInputStream(), Map.class);
             String username = credentials.get("username");
             String password = credentials.get("password");
 
-            // Create an authentication token
             UsernamePasswordAuthenticationToken authenticationToken =
                     new UsernamePasswordAuthenticationToken(username, password);
 
-            // Delegate authentication to the authentication manager
             return authenticationManager.authenticate(authenticationToken);
         } catch (IOException e) {
             throw new RuntimeException("Failed to parse authentication request", e);
@@ -45,7 +43,6 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
                                             FilterChain chain, Authentication authResult)
             throws IOException {
-        // Generate JWT token
         String token = JwtUtils.generateToken(authResult.getName());
         response.addHeader("Authorization", "Bearer " + token);
     }
